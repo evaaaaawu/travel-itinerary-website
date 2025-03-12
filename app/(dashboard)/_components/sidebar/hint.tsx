0886934d16cb88e5d1d79f353af8f968ca9e43cd
evaaaaawu <yiju.wu1@gmail.com>
2025-03-12 22:20:02 +0800

    feat: users can add different workspaces

---
+++ app/(dashboard)/_components/sidebar/hint.tsx
@@ -0,0 +1,45 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+export interface HintProps {
+  label: string;
+  children: React.ReactNode;
+  side?: "top" | "right" | "bottom" | "left";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+  alignOffset?: number;
+}
+
+export const Hint = ({ 
+  label, 
+  children, 
+  side = "bottom",
+  align = "start",
+  sideOffset = 0,
+  alignOffset = 0
+}: HintProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={100}>
+        <TooltipTrigger asChild>
+          {children}
+        </TooltipTrigger>
+        <TooltipContent
+          className="text-white bg-black border-black"
+          side={side} 
+          align={align} 
+          sideOffset={sideOffset} 
+          alignOffset={alignOffset}
+        >
+          <p className="font-semibold capitalize">
+            {label}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
