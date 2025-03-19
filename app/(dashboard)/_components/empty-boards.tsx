4b989bfaac7950246a0bca015ce0dbccb1492ab1
evaaaaawu <yiju.wu1@gmail.com>
2025-03-19 17:59:07 +0800

    feat: create mutation

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,29 @@
+"use client";
+
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
 import { Button } from "@/components/ui/button";
 import { LayoutDashboard } from "lucide-react";
+import { useOrganization } from "@clerk/nextjs";
+import { toast } from "sonner";
 
 export const EmptyBoards = () => {
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onclick = () => {
+    if (!organization) return;
+    mutate({
+      title: "New Board",
+      orgId: organization.id,
+    })
+      .then((id) => {
+        toast.success("Board created");
+        // TODO: Redirect to board/{id}
+      })
+      .catch(() => toast.error("Failed to create board"));
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-full">
       <LayoutDashboard className="w-15 h-15 text-muted-foreground" />
@@ -12,7 +34,7 @@ export const EmptyBoards = () => {
         Start by creating a board for your organization
       </p>
       <div className="mt-6">
-        <Button size="lg">
+        <Button size="lg" onClick={onclick} disabled={pending}>
           Create Board
         </Button>
       </div>
